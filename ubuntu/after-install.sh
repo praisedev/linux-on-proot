62ca0b11a1c0a4de9623b5bc14639474fb4d44d9
praisedev <62954475+praisedev@users.noreply.github.com>
2021-02-01 10:14:00 +0700

    Update after-install.sh

--- ubuntu/after-install.sh
+++ ubuntu/after-install.sh
@@ -3,16 +3,50 @@
 #Get the necessary components
 apt-mark hold udisks2
 [ ! -f /root/.parrot ] && apt-get update || echo "Parrot detected, not updating apt cache since that will break the whole distro"
-DEBIAN_FRONTEND=noninteractive apt-get install sudo wget -y
-DEBIAN_FRONTEND=noninteractive apt-get update && apt -y install software-properties-common && add-apt-repository ppa:ondrej/php -y && apt-get update && apt install php7.4 php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl mysql-server mysql-client -y
-
-#DEBIAN_FRONTEND=noninteractive apt-get install build-essential net-tools curl git software-properties-common -y
+#apt-get install keyboard-configuration -y
+apt-get install sudo wget -y
+apt-get install xfce4 xfce4-terminal tigervnc-standalone-server -y
+apt-get install xfe -y
 apt-get clean
+
+#Setup the necessary files
+mkdir ~/.vnc
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start -P /usr/local/bin/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-stop -P /usr/local/bin/
+
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vncserver-start
+chmod +x /usr/local/bin/vncserver-stop
+
+echo " "
 
 echo "Running browser patch"
 wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/ubchromiumfix.sh && chmod +x ubchromiumfix.sh
 ./ubchromiumfix.sh && rm -rf ubchromiumfix.sh
 
-# wget https://www.apachefriends.org/xampp-files/8.0.1/xampp-linux-x64-8.0.1-0-installer.run -O xampp.run
-# chmod +x xampp.run
-# ./xampp.run
+echo "You can now start vncserver by running vncserver-start"
+echo " "
+echo "It will ask you to enter a password when first time starting it."
+echo " "
+echo "The VNC Server will be started at 127.0.0.1:5901"
+echo " "
+echo "You can connect to this address with a VNC Viewer you prefer"
+echo " "
+echo "Connect to this address will open a window with Xfce4 Desktop Environment"
+echo " "
+echo " "
+echo " "
+echo "Running vncserver-start"
+echo " "
+echo " "
+echo " "
+echo "To Kill VNC Server just run vncserver-stop"
+echo " "
+echo " "
+echo " "
+
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+
+vncserver-start
